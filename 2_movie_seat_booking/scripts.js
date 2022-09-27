3fbe569e25b39f6fab50d41ad5cee71dada02708
wnicolass <nicolaswgl99@gmail.com>
2022-09-27 18:11:03 +0100

    feat: save data to ls

---
+++ 2_movie_seat_booking/scripts.js
@@ -0,0 +1,45 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+
+const movieSelect = document.getElementById("movie");
+let ticketPrice = +movieSelect.value;
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+function updateCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateCount();
+});
+
+// seat click event
+container.addEventListener("click", (e) => {
+  const isSeatAndNotOccupied =
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied");
+
+  if (isSeatAndNotOccupied) {
+    e.target.classList.toggle("selected");
+
+    updateCount();
+  }
+});
