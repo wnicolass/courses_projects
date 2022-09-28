6ca662608ab4b20fcd7ba1551e2926c070df7472
wnicolass <nicolaswgl99@gmail.com>
2022-09-28 15:28:54 +0100

    video player finished

--- 3_custom_video_player/script.js
+++ 3_custom_video_player/script.js
@@ -24,12 +24,20 @@ function updatePlayIcon() {
 
 //update progress bar and time
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  let minutes = Math.floor(video.currentTime / 60);
+  if (minutes < 10) minutes = "0" + String(minutes);
+
+  let seconds = Math.floor(video.currentTime % 60);
+  if (seconds < 10) seconds = "0" + String(seconds);
+
+  timestamp.innerHTML = `${minutes}:${seconds}`;
 }
 
 //set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 function stopVideo() {
