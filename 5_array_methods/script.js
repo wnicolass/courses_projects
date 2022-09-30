90899a98fb7237dfa88220db103f9dac7bb8b691
wnicolass <nicolaswgl99@gmail.com>
2022-09-30 22:26:27 +0100

    feat: show only millionaires

--- 5_array_methods/script.js
+++ 5_array_methods/script.js
@@ -1,7 +1,7 @@
 const main = document.getElementById("main");
 const addUserBtn = document.getElementById("add-user");
 const doubleBtn = document.getElementById("double");
-const showMillionariesBtn = document.getElementById("show-millionaires");
+const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
 
@@ -35,6 +35,11 @@ function doubleMoney() {
   updateDOM();
 }
 
+function showMillionaires() {
+  data = data.filter((data) => data.money > 1000000);
+  updateDOM();
+}
+
 function sortByRichest() {
   data.sort((a, b) => b.money - a.money);
   updateDOM();
@@ -65,4 +70,5 @@ function formatMoney(number) {
 
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
+showMillionairesBtn.addEventListener("click", showMillionaires);
 sortBtn.addEventListener("click", sortByRichest);
