f68b2ef10d69431db9767d5ec73b0f1e92765120
wnicolass <nicolaswgl99@gmail.com>
2022-10-01 14:56:41 +0100

    feat: toggle sidebar

--- 6_sidebar_modal/script.js
+++ 6_sidebar_modal/script.js
@@ -1,8 +1,16 @@
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
 const labels = document.querySelectorAll(".modal-form label");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("show-nav");
+});
 
 labels.forEach((label) => {
   label.innerHTML = label.innerText
     .split("")
-    .map((letter, idx) => `<span>${letter}</span>`)
+    .map((letter) => `<span>${letter}</span>`)
     .join("");
 });
