4ba828be2beac639d1a30aec2098289521c5e790
wnicolass <nicolaswgl99@gmail.com>
2022-10-01 15:41:06 +0100

    feat: close sidebar when click outside

--- 6_sidebar_modal/script.js
+++ 6_sidebar_modal/script.js
@@ -2,10 +2,27 @@ const toggle = document.getElementById("toggle");
 const close = document.getElementById("close");
 const open = document.getElementById("open");
 const modal = document.getElementById("modal");
+const navbar = document.querySelector("body nav");
 const labels = document.querySelectorAll(".modal-form label");
+
+function closeNavBar(e) {
+  if (
+    document.body.classList.contains("show-nav") &&
+    e.target !== toggle &&
+    !toggle.contains(e.target) &&
+    e.target !== navbar &&
+    !navbar.contains(e.target)
+  ) {
+    document.body.classList.toggle("show-nav");
+    document.body.removeEventListener("click", closeNavbar);
+  } else if (!document.body.classList.contains("show-nav")) {
+    document.body.removeEventListener("click", closeNavbar);
+  }
+}
 
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("show-nav");
+  document.body.addEventListener("click", closeNavBar);
 });
 
 open.addEventListener("click", () => modal.classList.add("show-modal"));
