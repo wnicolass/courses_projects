78791bfddac7a9ac698dd121aec38b26fe173336
wnicolass <nicolaswgl99@gmail.com>
2022-10-02 15:01:47 +0100

    feat: display word function

---
+++ 7_hangman/script.js
@@ -0,0 +1,46 @@
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = [
+  "application",
+  "programming",
+  "interface",
+  "wizard",
+  "javascript",
+  "react",
+  "node",
+];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = ["r", "e", "a", "c", "t"];
+const wrongLettrs = [];
+
+function displayWord() {
+  wordEl.innerHTML = `
+        ${selectedWord
+          .split("")
+          .map(
+            (letter) =>
+              `<span class="letter">${
+                correctLetters.includes(letter) ? letter : ""
+              }</span>`
+          )
+          .join("")}
+    `;
+
+  const innerWord = wordEl.innerText.replace(/\n/g, "");
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations! You won! 😃";
+    popup.style.display = "flex";
+  }
+}
+
+displayWord();
