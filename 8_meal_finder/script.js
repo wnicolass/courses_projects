f029827df389e6a43874a6e597274aa6232abbe7
wnicolass <nicolaswgl99@gmail.com>
2022-10-03 21:55:46 +0100

    feat: populate UI with data

--- 8_meal_finder/script.js
+++ 8_meal_finder/script.js
@@ -17,8 +17,27 @@ function searchMeal(e) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        resultHeading.innerHTML = `<h2>Search results for "${term}":</h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results. Try again! 😢</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+            <div class="meal">
+                <img src="${meal.strMealThumb}" alt ="${meal.strMeal}" />
+                <div class="meal-info" data-mealID="${meal.idMeal}">
+                    <h3>${meal.strMeal}</h3>
+                </div>
+            </div>
+            `
+            )
+            .join("");
+        }
       });
+
+    search.value = "";
   } else {
     //todo: notification
     alert("Please enter a search term");
