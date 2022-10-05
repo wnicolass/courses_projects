d960a4bdcdb3d4625e5f6dbcac9d0ce02285970b
wnicolass <nicolaswgl99@gmail.com>
2022-10-05 15:52:44 +0100

    feat: get data from ls

--- 9_expense_tracker/script.js
+++ 9_expense_tracker/script.js
@@ -7,14 +7,13 @@ const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 const small = document.getElementById("error");
 
-const dummyTransactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
-];
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
 
-let transactions = dummyTransactions;
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
+
 let count = 0;
 
 function addTransaction(e) {
