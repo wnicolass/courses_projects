2fd67863dfbfdf06ffd576c7d104fced4033a84c
wnicolass <nicolaswgl99@gmail.com>
2022-10-06 16:44:20 +0100

    feat: play and pause music

--- 10_music_player/script.js
+++ 10_music_player/script.js
@@ -14,9 +14,34 @@ const songs = ["high_enough", "cowbel_warrior", "miss_the_rage"];
 let songIndex = 2;
 
 function loadSong(song) {
-  title.innerText = song;
+  title.innerText = song.replace(/_/g, " ");
   audio.src = `./music/${song}.mp3`;
   cover.src = `./images/${song}.png`;
 }
 
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
 loadSong(songs[songIndex]);
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
