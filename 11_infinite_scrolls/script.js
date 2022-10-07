176a96de6d700290918658110f9b2a4897aebba8
wnicolass <nicolaswgl99@gmail.com>
2022-10-07 16:53:30 +0100

    feat: fetch more posts when scroll :sparkles:

--- 11_infinite_scrolls/script.js
+++ 11_infinite_scrolls/script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById("post-container");
 const loading = document.querySelector(".loader");
 const filter = document.getElementById("filter");
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 async function getPosts() {
@@ -33,4 +33,25 @@ async function showPosts() {
   });
 }
 
+function showLoading() {
+  loading.classList.add("show");
+
+  setTimeout(() => {
+    loading.classList.remove("show");
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
 showPosts();
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
