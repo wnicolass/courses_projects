843382a9a32ce030ac3ebabc50146702abaaf839
wnicolass <nicolaswgl99@gmail.com>
2022-10-07 15:37:11 +0100

    feat: fetch posts data from api

---
+++ 11_infinite_scrolls/script.js
@@ -0,0 +1,16 @@
+const postsContainer = document.getElementById("posts-container");
+const loading = document.querySelector(".loader");
+const filter = document.getElementById("filter");
+
+let limit = 3;
+let page = 1;
+
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+
+  return data;
+}
