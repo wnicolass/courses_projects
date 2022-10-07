43ca7c4e5cc4516505268e9ea6c9cd17509da06b
wnicolass <nicolaswgl99@gmail.com>
2022-10-07 15:42:19 +0100

    feat: populate UI with posts :sparkles:

--- 11_infinite_scrolls/script.js
+++ 11_infinite_scrolls/script.js
@@ -1,4 +1,4 @@
-const postsContainer = document.getElementById("posts-container");
+const postsContainer = document.getElementById("post-container");
 const loading = document.querySelector(".loader");
 const filter = document.getElementById("filter");
 
@@ -14,3 +14,23 @@ async function getPosts() {
 
   return data;
 }
+
+async function showPosts() {
+  const posts = await getPosts();
+
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `
+        <div class="number">${post.id}</div>
+        <div class="post-info">
+            <h2 class="post-title">${post.title}</h2>
+            <p class="post-body">${post.body}</p>
+        </div>
+    `;
+
+    postsContainer.appendChild(postEl);
+  });
+}
+
+showPosts();
