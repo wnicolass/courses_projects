9a24aa8ff6771a672ced610ba07188ebaa9f8a4f
wnicolass <nicolaswgl99@gmail.com>
2022-10-09 11:53:56 +0100

    feat: populate UI with images

---
+++ 13_text_reader/script.js
@@ -0,0 +1,72 @@
+const main = document.querySelector("main");
+const voicesSelect = document.getElementById("voices");
+const textarea = document.getElementById("text");
+const readBtn = document.getElementById("read");
+const toggleBtn = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
+
+const data = [
+  {
+    image: "./assets/images/drink.jpg",
+    text: "I'm thirsty",
+  },
+  {
+    image: "./assets/images/food.jpg",
+    text: "I'm Hungry",
+  },
+  {
+    image: "./assets/images/tired.jpg",
+    text: "I'm Tired",
+  },
+  {
+    image: "./assets/images/hurt.jpg",
+    text: "I'm Hurt",
+  },
+  {
+    image: "./assets/images/happy.jpg",
+    text: "I'm Happy",
+  },
+  {
+    image: "./assets/images/angry.jpg",
+    text: "I'm Angry",
+  },
+  {
+    image: "./assets/images/sad.jpg",
+    text: "I'm Sad",
+  },
+  {
+    image: "./assets/images/scared.jpg",
+    text: "I'm Scared",
+  },
+  {
+    image: "./assets/images/outside.jpg",
+    text: "I Want To Go Outside",
+  },
+  {
+    image: "./assets/images/home.jpg",
+    text: "I Want To Go Home",
+  },
+  {
+    image: "./assets/images/school.jpg",
+    text: "I Want To Go To School",
+  },
+  {
+    image: "./assets/images/grandma.jpg",
+    text: "I Want To Go To Grandmas",
+  },
+];
+
+function createBox(item) {
+  const box = document.createElement("div");
+  const { image, text } = item;
+
+  box.classList.add("box");
+  box.innerHTML = `
+        <img src="${image}" alt="${text}" />
+        <p class="info">${text}</p>
+    `;
+
+  main.appendChild(box);
+}
+
+data.forEach(createBox);
