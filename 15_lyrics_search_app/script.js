31e2f60f911164e2664ed5cd55507650b7104371
wnicolass <nicolaswgl99@gmail.com>
2022-10-11 17:58:53 +0100

    feat: populate UI with results

--- 15_lyrics_search_app/script.js
+++ 15_lyrics_search_app/script.js
@@ -5,7 +5,37 @@ const more = document.getElementById("more");
 
 const API_URL = "https://api.lyrics.ovh";
 
-function showData(data) {}
+function showData(songs) {
+  result.innerHTML = `
+    <ul class="songs">
+        ${songs.data
+          .map(
+            (song) => `<li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    </li>`
+          )
+          .join("")}
+    </ul>
+  `;
+
+  if (songs.prev || songs.next) {
+    more.innerHTML = `
+        ${
+          songs.prev
+            ? `<button class="btn" onclick="getMoreSongs('${songs.prev}')">Prev</button>`
+            : ""
+        }
+        ${
+          songs.next
+            ? `<button class="btn" onclick="getMoreSongs('${songs.next}')">Next</button>`
+            : ""
+        }
+    `;
+  } else {
+    more.innerHTML = "";
+  }
+}
 
 async function searchSongs(term) {
   const res = await fetch(`${API_URL}/suggest/${term}`);
