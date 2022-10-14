cc133d9b40da7a0cabc8541e15c758286d07c339
wnicolass <nicolaswgl99@gmail.com>
2022-10-14 08:31:05 +0100

    feat: create bricks properties :construction:

--- 17_breakout_game/script.js
+++ 17_breakout_game/script.js
@@ -4,6 +4,9 @@ const rules = document.getElementById("rules");
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColumnCount = 9;
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -26,6 +29,26 @@ const paddle = {
   speed: 8,
   dx: 0,
 };
+
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+const bricks = [];
+
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
 
 function drawPaddle() {
   ctx.beginPath();
