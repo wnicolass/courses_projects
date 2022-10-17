d969de16592d5db56db3051cd8fb5982b929e30e
wnicolass <nicolaswgl99@gmail.com>
2022-10-17 21:52:41 +0100

    feat: check the value said by the user :sparkles:

--- 20_speak_number/script.js
+++ 20_speak_number/script.js
@@ -16,6 +16,32 @@ function writeMessage(msg) {
     `;
 }
 
+function checkNumber(msg) {
+  const num = +msg;
+
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML = "<div>That is not a valid number</div>";
+    return;
+  }
+
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += "<div>Number must be between 1 and 100</div>";
+    return;
+  }
+
+  if (num === randomNum) {
+    document.body.innerHTML = `
+        <h2>Congrats! You have guessed the number! <br><br>
+        It was ${num}</h2>
+        <button class="play-again" id="play-again">Play Again!</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += "<div>GO LOWER</div>";
+  } else {
+    msgEl.innerHTML += "<div>GO HIGHER</div>";
+  }
+}
+
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
