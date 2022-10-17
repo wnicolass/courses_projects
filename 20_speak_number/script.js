cad0a9837ece346faa470c8533346dbd67b2df22
wnicolass <nicolaswgl99@gmail.com>
2022-10-17 21:34:04 +0100

    feat: get user's voice with speech recognition :sparkles:

---
+++ 20_speak_number/script.js
@@ -0,0 +1,23 @@
+const msgEl = document.getElementById("msg");
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+function onSpeak(e) {
+  const msg = e.results[0][0].transcript;
+
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+recognition.start();
+
+const randomNum = getRandomNumber(1, 100);
+
+recognition.addEventListener("result", onSpeak);
