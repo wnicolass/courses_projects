12362ece0348e79c41b64d7050189c067e5b02a4
wnicolass <nicolaswgl99@gmail.com>
2022-10-19 19:32:36 +0100

    feat: fetch quotes from api :sparkles:

--- 21_quote_generator/script.js
+++ 21_quote_generator/script.js
@@ -1,1 +1,15 @@
+let apiQuotes = [];
 
+async function getQuotes() {
+  const apiURL = `https://jacintodesign.github.io/quotes-api/data/quotes.json`;
+
+  try {
+    const res = await fetch(apiURL);
+    apiQuotes = await res.json();
+    console.log(apiQuotes);
+  } catch (err) {
+    alert("Fail to get the quotes");
+  }
+}
+
+getQuotes();
