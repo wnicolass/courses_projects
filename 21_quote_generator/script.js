556f5fb14f9b938a63d6d70a5ef8395302cd9511
wnicolass <nicolaswgl99@gmail.com>
2022-10-19 20:07:51 +0100

    feat: generate random quote :sparkles:

--- 21_quote_generator/script.js
+++ 21_quote_generator/script.js
@@ -1,12 +1,19 @@
 let apiQuotes = [];
 
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length) + 1];
+  console.log(quote);
+}
+
+// Get data from API
 async function getQuotes() {
   const apiURL = `https://jacintodesign.github.io/quotes-api/data/quotes.json`;
 
   try {
     const res = await fetch(apiURL);
     apiQuotes = await res.json();
-    console.log(apiQuotes);
+
+    newQuote();
   } catch (err) {
     alert("Fail to get the quotes");
   }
