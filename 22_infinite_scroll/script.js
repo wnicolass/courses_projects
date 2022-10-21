47522ab1e2dc2f300b598063171897467127f088
wnicolass <nicolaswgl99@gmail.com>
2022-10-21 14:02:12 +0100

    feat: populate UI with images :sparkles:

--- 22_infinite_scroll/script.js
+++ 22_infinite_scroll/script.js
@@ -1,13 +1,48 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photosArray = [];
+
 const count = 10;
 const apiKey = "QZUJo-bFWJcCXUlJF-GATUdzxk58NdC5IO80FMI7TOg";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function showPhotos() {
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      taget: "_blank",
+    });
+
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: !photo.alt_description
+        ? "imagine description here"
+        : photo.alt_description,
+      title: !photo.alt_description
+        ? "imagine title here"
+        : photo.alt_description,
+    });
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
 async function getPhotos() {
   try {
     const res = await fetch(apiUrl);
-    const data = await res.json();
+    photosArray = await res.json();
 
-    console.log(data);
+    showPhotos();
   } catch (err) {
     console.log(err);
   }
