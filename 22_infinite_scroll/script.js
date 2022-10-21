29478988544490b705a98409a0c9b5759ef46c9a
wnicolass <nicolaswgl99@gmail.com>
2022-10-21 13:16:18 +0100

    feat: get photos from unsplash api :sparkles:

--- 22_infinite_scroll/script.js
+++ 22_infinite_scroll/script.js
@@ -1,1 +1,16 @@
+const count = 10;
+const apiKey = "QZUJo-bFWJcCXUlJF-GATUdzxk58NdC5IO80FMI7TOg";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+async function getPhotos() {
+  try {
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getPhotos();
