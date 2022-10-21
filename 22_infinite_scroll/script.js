41e65288a3cb1bf838e98d16ee4d9b4e7478a2a9
wnicolass <nicolaswgl99@gmail.com>
2022-10-21 14:12:53 +0100

    feat: show more images when scroll :sparkles:

--- 22_infinite_scroll/script.js
+++ 22_infinite_scroll/script.js
@@ -48,4 +48,13 @@ async function getPhotos() {
   }
 }
 
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhotos();
+  }
+});
+
 getPhotos();
