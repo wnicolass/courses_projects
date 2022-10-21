2644c4a974ffdb020b582cc2a5663f75cc2a87d3
wnicolass <nicolaswgl99@gmail.com>
2022-10-21 14:29:10 +0100

    fix: not load images when reach bottom :bug:

--- 22_infinite_scroll/script.js
+++ 22_infinite_scroll/script.js
@@ -1,11 +1,24 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
-const count = 10;
+const count = 30;
 const apiKey = "QZUJo-bFWJcCXUlJF-GATUdzxk58NdC5IO80FMI7TOg";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    console.log("ready =", ready);
+    imagesLoaded = 0;
+  }
+}
 
 function setAttributes(element, attributes) {
   for (const key in attributes) {
@@ -14,6 +27,8 @@ function setAttributes(element, attributes) {
 }
 
 function showPhotos() {
+  totalImages = photosArray.length;
+  console.log("total images=", totalImages);
   photosArray.forEach((photo) => {
     const item = document.createElement("a");
     setAttributes(item, {
@@ -31,6 +46,8 @@ function showPhotos() {
         ? "imagine title here"
         : photo.alt_description,
     });
+
+    img.addEventListener("load", imageLoaded);
 
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -50,9 +67,10 @@ async function getPhotos() {
 
 window.addEventListener("scroll", () => {
   if (
-    window.innerHeight + window.scrollY >=
-    document.body.offsetHeight - 1000
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
   ) {
+    ready = false;
     getPhotos();
   }
 });
