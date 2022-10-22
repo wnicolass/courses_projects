b8ac2872670687e5170c4a15db94d445090ab7bd
wnicolass <nicolaswgl99@gmail.com>
2022-10-22 14:54:00 +0100

    feat: add picture in picture functionality :sparkles:

--- 23_picture_in_picture/script.js
+++ 23_picture_in_picture/script.js
@@ -1,1 +1,24 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
 
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+button.addEventListener("click", async () => {
+  button.disabled = true;
+
+  await videoElement.requestPictureInPicture();
+
+  button.disabled = false;
+});
+
+selectMediaStream();
