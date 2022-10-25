9d5e378b5a32b5f99a3954b610cf4ab28ddafd9f
wnicolass <nicolaswgl99@gmail.com>
2022-10-25 16:24:22 +0100

    refactor: optimize set theme mode function :sparkles:

--- 25_light_dark_mode_website/script.js
+++ 25_light_dark_mode_website/script.js
@@ -1,15 +1,15 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById("nav");
 const toggleIcon = document.getElementById("toggle-icon");
-const image1 = document.getElementById("image1");
-const image2 = document.getElementById("image2");
-const image3 = document.getElementById("image3");
+const images = document.querySelectorAll(".image");
 const textBox = document.getElementById("text-box");
 
-function themeMode(color) {
-  image1.src = `assets/images/undraw_proud_coder_${color}.svg`;
-  image2.src = `assets/images/undraw_feeling_proud_${color}.svg`;
-  image3.src = `assets/images/undraw_conceptual_idea_${color}.svg`;
+function themeMode(theme) {
+  [img1, img2, img3] = images;
+
+  img1.src = `assets/images/undraw_proud_coder_${theme}.svg`;
+  img2.src = `assets/images/undraw_feeling_proud_${theme}.svg`;
+  img3.src = `assets/images/undraw_conceptual_idea_${theme}.svg`;
 }
 
 function toggleDarkLightMode(isDark) {
