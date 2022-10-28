b8fc157445c1d9fa8c918823a22a6f9bc97dfc7f
wnicolass <nicolaswgl99@gmail.com>
2022-10-28 15:26:11 +0100

    feat: open and close the navigation bar :sparkles:

--- 27_navigation_nation/script.js
+++ 27_navigation_nation/script.js
@@ -8,6 +8,15 @@ const nav5 = document.getElementById("nav-5");
 
 function toggleNav() {
   menuBars.classList.toggle("change");
+
+  overlay.classList.toggle("overlay-active");
+  if (overlay.classList.contains("overlay-active")) {
+    overlay.classList.remove("overlay-slide-left");
+    overlay.classList.add("overlay-slide-right");
+  } else {
+    overlay.classList.remove("overlay-slide-right");
+    overlay.classList.add("overlay-slide-left");
+  }
 }
 
 menuBars.addEventListener("click", toggleNav);
