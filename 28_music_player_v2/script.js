3721a3cc693a133a42d086b6f6ef8f3fecea4de6
wnicolass <nicolaswgl99@gmail.com>
2022-10-30 11:15:00 +0000

    feat: update progress bar :sparkles:

--- 28_music_player_v2/script.js
+++ 28_music_player_v2/script.js
@@ -2,6 +2,8 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -80,5 +82,16 @@ function prevSong() {
 
 loadSong(songs[currentSongIndex]);
 
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+}
+
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+
+music.addEventListener("timeupdate", updateProgressBar);
