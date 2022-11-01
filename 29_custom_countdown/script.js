6a720eac6e21ad42064cfaef0a8099ed3eda3e8c
wnicolass <nicolaswgl99@gmail.com>
2022-11-01 11:14:08 +0000

    feat: get difference between countdown date and now :sparkles:

--- 29_custom_countdown/script.js
+++ 29_custom_countdown/script.js
@@ -2,11 +2,23 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdown-form");
 const dateEl = document.getElementById("date-picker");
 
+const countdownEl = document.getElementById("countdown");
+const countdownElTitle = document.getElementById("countdown-title");
+const countdownBtn = document.getElementById("countdown-button");
+const timeElements = document.querySelectorAll("span");
+
 let countdownTitle = "";
 let countdownDate = "";
+let countdownValue = Date;
 
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+function updateDOM() {
+  const now = new Date().getTime();
+  const diff = countdownValue - now;
+  console.log(diff);
+}
 
 function updateCountdown(e) {
   e.preventDefault();
@@ -15,6 +27,10 @@ function updateCountdown(e) {
     e.srcElement[1].value,
   ];
   console.log(countdownTitle, countdownDate);
+
+  countdownValue = new Date(countdownDate).getTime();
+  console.log("countdown value:", countdownValue);
+  updateDOM();
 }
 
 countdownForm.addEventListener("submit", updateCountdown);
