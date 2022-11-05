9cd20b998eb60b6572842220b1f929d51a3f7581
wnicolass <nicolaswgl99@gmail.com>
2022-11-05 12:18:30 +0000

    feat: play and pause video :sparkles:

--- 31_video_player_v2/script.js
+++ 31_video_player_v2/script.js
@@ -1,21 +1,30 @@
+const player = document.querySelector(".player");
+const video = document.querySelector(".video");
+const progressRange = document.querySelector(".progress-range");
+const progressBar = document.querySelector(".progress-bar");
+const playBtn = document.getElementById("play-btn");
+const volumeIcon = document.getElementById("volume-icon");
+const volumeRange = document.querySelector(".volume-range");
+const volumeBar = document.querySelector(".volume-bar");
+const speed = document.querySelector(".player-speed");
+const currentTime = document.querySelector(".time-elapsed");
+const duration = document.querySelector(".time-duration");
+const fullscreenBtn = document.querySelector(".fullscreen");
 
+function showPlayIcon() {
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+}
 
-// Play & Pause ----------------------------------- //
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
+  } else {
+    video.pause();
+  }
+}
 
-
-
-// Progress Bar ---------------------------------- //
-
-
-
-// Volume Controls --------------------------- //
-
-
-
-// Change Playback Speed -------------------- //
-
-
-
-// Fullscreen ------------------------------- //
-
-
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
