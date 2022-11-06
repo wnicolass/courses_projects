4a7e857fa3244ae58b036846714f3b154931a18b
wnicolass <nicolaswgl99@gmail.com>
2022-11-06 12:23:23 +0000

    feat: change video volume :sparkles:

--- 31_video_player_v2/script.js
+++ 31_video_player_v2/script.js
@@ -52,9 +52,32 @@ function setProgress(e) {
   video.currentTime = newTime * video.duration;
 }
 
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+
+  if (volume < 0.1) {
+    volume = 0;
+  }
+  if (volume > 0.9) {
+    volume = 1;
+  }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+
+  volumeIcon.className = "";
+  if (volume > 0.7) {
+    volumeIcon.classList.add("fas", "fa-volume-up");
+  } else if (volume > 0) {
+    volumeIcon.classList.add("fas", "fa-volume-down");
+  } else if (volume === 0) {
+    volumeIcon.classList.add("fas", "fa-volume-off");
+  }
+}
+
 video.addEventListener("ended", showPlayIcon);
 playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
+volumeRange.addEventListener("click", changeVolume);
