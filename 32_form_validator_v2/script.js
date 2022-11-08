29e73cb2f4c4f25a44d7b2c99286379638002a4e
wnicolass <nicolaswgl99@gmail.com>
2022-11-08 15:15:15 +0000

    feat: validate password :sparkles:

--- 32_form_validator_v2/script.js
+++ 32_form_validator_v2/script.js
@@ -5,13 +5,29 @@ const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
   isValid = form.checkValidity();
 
-  message.textContent = "Please fill out all fields.";
-  message.style.color = "red";
-  messageContainer.style.borderColor = "red";
+  if (!isValid) {
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+  }
+
+  if (password1.value === password2.value) {
+    passwordsMatch = true;
+    password1.style.borderColor = "green";
+    password2.style.borderColor = "green";
+  } else {
+    passwordsMatch = false;
+    message.textContent = "Make sure passwords match.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1.style.borderColor = "red";
+    password2.style.borderColor = "red";
+  }
 }
 
 function processFormData(e) {
