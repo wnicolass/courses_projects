ebb2df028a140661002f07ee591765d2a5ac93a4
wnicolass <nicolaswgl99@gmail.com>
2022-11-11 14:30:09 +0000

    feat: select icon :sparkles:

--- 33_spock_rock/script.js
+++ 33_spock_rock/script.js
@@ -1,10 +1,61 @@
+const playerScoreEl = document.getElementById("player-score");
+const playerChoiceEl = document.getElementById("player-choice");
+const computerScoreEl = document.getElementById("computer-score");
+const computerChoiceEl = document.getElementById("computer-choice");
 
+const playerRock = document.getElementById("player-rock");
+const playerPaper = document.getElementById("player-paper");
+const playerScissors = document.getElementById("player-scissors");
+const playerLizard = document.getElementById("player-lizard");
+const playerSpock = document.getElementById("player-spock");
+
+const computerRock = document.getElementById("computer-rock");
+const computerPaper = document.getElementById("computer-paper");
+const computerScissors = document.getElementById("computer-scissors");
+const computerLizard = document.getElementById("computer-lizard");
+const computerSpock = document.getElementById("computer-spock");
+
+const allGameIcons = document.querySelectorAll(".far");
+const resultText = document.getElementById("result-text");
 
 const choices = {
-  rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
-  paper: { name: 'Paper', defeats: ['rock', 'spock'] },
-  scissors: { name: 'Scissors', defeats: ['paper', 'lizard'] },
-  lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
-  spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
+  rock: { name: "Rock", defeats: ["scissors", "lizard"] },
+  paper: { name: "Paper", defeats: ["rock", "spock"] },
+  scissors: { name: "Scissors", defeats: ["paper", "lizard"] },
+  lizard: { name: "Lizard", defeats: ["paper", "spock"] },
+  spock: { name: "Spock", defeats: ["scissors", "rock"] },
 };
 
+function resetSelected() {
+  allGameIcons.forEach((icon) => {
+    icon.classList.remove("selected");
+  });
+}
+
+function select(playerChoice) {
+  resetSelected();
+  switch (playerChoice) {
+    case "rock":
+      playerRock.classList.add("selected");
+      playerChoiceEl.textContent = " --- Rock";
+      break;
+    case "paper":
+      playerPaper.classList.add("selected");
+      playerChoiceEl.textContent = " --- Paper";
+      break;
+    case "scissors":
+      playerScissors.classList.add("selected");
+      playerChoiceEl.textContent = " --- Scissors";
+      break;
+    case "lizard":
+      playerLizard.classList.add("selected");
+      playerChoiceEl.textContent = " --- Lizard";
+      break;
+    case "spock":
+      playerSpock.classList.add("selected");
+      playerChoiceEl.textContent = " --- Spock";
+      break;
+    default:
+      break;
+  }
+}
