532585681d29476f0748de54f33c8ebf17db1e38
wnicolass <nicolaswgl99@gmail.com>
2022-11-16 14:04:03 +0000

    feat: select amoun of questions :sparkles:

--- 35_math_sprint_game/script.js
+++ 35_math_sprint_game/script.js
@@ -1,22 +1,22 @@
 // Pages
-const gamePage = document.getElementById('game-page');
-const scorePage = document.getElementById('score-page');
-const splashPage = document.getElementById('splash-page');
-const countdownPage = document.getElementById('countdown-page');
+const gamePage = document.getElementById("game-page");
+const scorePage = document.getElementById("score-page");
+const splashPage = document.getElementById("splash-page");
+const countdownPage = document.getElementById("countdown-page");
 // Splash Page
-const startForm = document.getElementById('start-form');
-const radioContainers = document.querySelectorAll('.radio-container');
-const radioInputs = document.querySelectorAll('input');
-const bestScores = document.querySelectorAll('.best-score-value');
+const startForm = document.getElementById("start-form");
+const radioContainers = document.querySelectorAll(".radio-container");
+const radioInputs = document.querySelectorAll("input");
+const bestScores = document.querySelectorAll(".best-score-value");
 // Countdown Page
-const countdown = document.querySelector('.countdown');
+const countdown = document.querySelector(".countdown");
 // Game Page
-const itemContainer = document.querySelector('.item-container');
+const itemContainer = document.querySelector(".item-container");
 // Score Page
-const finalTimeEl = document.querySelector('.final-time');
-const baseTimeEl = document.querySelector('.base-time');
-const penaltyTimeEl = document.querySelector('.penalty-time');
-const playAgainBtn = document.querySelector('.play-again');
+const finalTimeEl = document.querySelector(".final-time");
+const baseTimeEl = document.querySelector(".base-time");
+const penaltyTimeEl = document.querySelector(".penalty-time");
+const playAgainBtn = document.querySelector(".play-again");
 
 // Equations
 
@@ -35,13 +35,13 @@ const wrongFormat = [];
 // Create Correct/Incorrect Random Equations
 function createEquations() {
   // Randomly choose how many correct equations there should be
-  // const correctEquations = 
+  // const correctEquations =
   // Set amount of wrong equations
-  // const wrongEquations = 
+  // const wrongEquations =
   // Loop through, multiply random numbers up to 9, push to array
   // for (let i = 0; i < correctEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
+  //   firstNumber =
+  //   secondNumber =
   //   const equationValue = firstNumber * secondNumber;
   //   const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
   //   equationObject = { value: equation, evaluated: 'true' };
@@ -49,13 +49,13 @@ function createEquations() {
   // }
   // Loop through, mess with the equation results, push to array
   // for (let i = 0; i < wrongEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
+  //   firstNumber =
+  //   secondNumber =
   //   const equationValue = firstNumber * secondNumber;
   //   wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
   //   wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
   //   wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
-  //   const formatChoice = 
+  //   const formatChoice =
   //   const equation = wrongFormat[formatChoice];
   //   equationObject = { value: equation, evaluated: 'false' };
   //   equationsArray.push(equationObject);
@@ -82,3 +82,14 @@ function createEquations() {
 //   bottomSpacer.classList.add('height-500');
 //   itemContainer.appendChild(bottomSpacer);
 // }
+
+startForm.addEventListener("click", () => {
+  radioContainers.forEach((radioEl) => {
+    radioEl.classList.remove("selected-label");
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add("selected-label");
+    }
+  });
+});
+
+startForm.addEventListener("click", selectQuestionAmount);
