f021c9dced8cd8a1ed16d253d982c82d4208d21b
wnicolass <nicolaswgl99@gmail.com>
2022-11-21 20:09:41 +0000

    feat: get operator for calcs :sparkles:

--- 37_calculator/script.js
+++ 37_calculator/script.js
@@ -2,29 +2,59 @@ const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear");
 
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
-  const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent =
-    displayValue === "0" ? number : displayValue + number;
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
 }
 
 function addDecimal() {
+  if (awaitingNextValue) {
+    return;
+  }
+
   if (!calculatorDisplay.textContent.includes(".")) {
     calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
   }
+}
+
+function useOperator(operator) {
+  const currentValue = +calculatorDisplay.textContent;
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    console.log("current value", currentValue);
+  }
+  awaitingNextValue = true;
+  operatorValue = operator;
+
+  console.log("first value", firstValue);
+  console.log("operator", operatorValue);
 }
 
 inputBtns.forEach((btn) => {
   if (btn.classList.length === 0) {
     btn.addEventListener("click", () => sendNumberValue(btn.value));
   } else if (btn.classList.contains("operator")) {
-    btn.addEventListener("click", () => sendNumberValue(btn.value));
+    btn.addEventListener("click", () => useOperator(btn.value));
   } else if (btn.classList.contains("decimal")) {
     btn.addEventListener("click", addDecimal);
   }
 });
 
 function resetAll() {
+  firstValue = 0;
+  operatorValue = "";
+  awaitingNextValue = false;
   calculatorDisplay.textContent = "0";
 }
 
