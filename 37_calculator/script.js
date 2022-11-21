cee0f76b8d1c3444bdc86e4b4f360537dece5bdf
wnicolass <nicolaswgl99@gmail.com>
2022-11-21 14:38:35 +0000

    feat: get numbers from input :sparkles:

---
+++ 37_calculator/script.js
@@ -0,0 +1,19 @@
+const calculatorDisplay = document.querySelector("h1");
+const inputBtns = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear-btn");
+
+function sendNumberValue(number) {
+  const displayValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent =
+    displayValue === "0" ? number : displayValue + number;
+}
+
+inputBtns.forEach((btn) => {
+  if (btn.classList.length === 0) {
+    btn.addEventListener("click", () => sendNumberValue(btn.value));
+  } else if (btn.classList.contains("operator")) {
+    btn.addEventListener("click", () => sendNumberValue(btn.value));
+  } else if (btn.classList.contains("decimal")) {
+    btn.addEventListener("click", () => sendNumberValue(btn.value));
+  }
+});
