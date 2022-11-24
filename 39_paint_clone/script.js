181686bbf0de532c5686a5fcd4334d24a219891b
wnicolass <nicolaswgl99@gmail.com>
2022-11-24 19:40:57 +0000

    feat: set brush color :sparkles:

--- 39_paint_clone/script.js
+++ 39_paint_clone/script.js
@@ -16,10 +16,11 @@ const { body } = document;
 const canvas = document.createElement("canvas");
 canvas.id = "canvas";
 const context = canvas.getContext("2d");
+
 let currentSize = 10;
 let bucketColor = "#FFFFFF";
 let currentColor = "#A51DAB";
-// let isEraser = false;
+let isEraser = false;
 // let isMouseDown = false;
 // let drawnArray = [];
 
@@ -34,9 +35,10 @@ let currentColor = "#A51DAB";
 // });
 
 // Setting Brush Color
-// brushColorBtn.addEventListener('change', () => {
-
-// });
+brushColorBtn.addEventListener("change", () => {
+  isEraser = false;
+  currentColor = `#${brushColorBtn.value}`;
+});
 
 // Setting Background Color
 bucketColorBtn.addEventListener("change", () => {
