97b1d51f95c5f0dfac5bd52132f5a9102d664102
wnicolass <nicolaswgl99@gmail.com>
2022-11-29 20:40:46 +0000

    feat: add animation on ball :sparkles:

--- 40_pong_game/script.js
+++ 40_pong_game/script.js
@@ -89,9 +89,6 @@ function createCanvas() {
   renderCanvas();
 }
 
-// Remove this
-createCanvas();
-
 // Reset Ball to Center
 function ballReset() {
   ballX = width / 2;
@@ -204,6 +201,7 @@ function animate() {
   ballMove();
   ballBoundaries();
   computerAI();
+  window.requestAnimationFrame(animate);
 }
 
 // Start Game, Reset Everything
@@ -219,7 +217,6 @@ function startGame() {
   createCanvas();
   animate();
   canvas.addEventListener("mousemove", (e) => {
-    console.log(e.clientX);
     playerMoved = true;
     // Compensate for canvas being centered
     paddleBottomX = e.clientX - canvasPosition - paddleDiff;
@@ -234,5 +231,4 @@ function startGame() {
   });
 }
 
-// On Load
-// startGame();
+startGame();
