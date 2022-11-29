aae3d26d219a5b9aa433e32451a16778c1b158de
wnicolass <nicolaswgl99@gmail.com>
2022-11-29 20:06:16 +0000

    feat: render canvas :sparkles:

--- 40_pong_game/script.js
+++ 40_pong_game/script.js
@@ -50,13 +50,16 @@ const winningScore = 7;
 // Render Everything on Canvas
 function renderCanvas() {
   // Canvas Background
-  // context.fillStyle = 'black';
-  // context.fillRect(0, 0, width, height);
+  context.fillStyle = "black";
+  context.fillRect(0, 0, width, height);
+
   // Paddle Color
   // context.fillStyle = 'white';
+
   // Player Paddle (Bottom)
   // context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
   // Computer Paddle (Top)
+
   // context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight);
   // Dashed Center Line
   // context.beginPath();
@@ -65,11 +68,13 @@ function renderCanvas() {
   // context.lineTo(500, 350);
   // context.strokeStyle = 'grey';
   // context.stroke();
+
   // Ball
   // context.beginPath();
   // context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
   // context.fillStyle = 'white';
   // context.fill();
+
   // Score
   // context.font = '32px Courier New';
   // context.fillText(playerScore, 20, canvas.height / 2 + 50);
